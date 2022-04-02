1eb58952dea2aa988299a1c320c471ff5819ea31
rockey17 <81315057+rockey17@users.noreply.github.com>
2022-04-02 17:38:10 +0530

    Update afoot.py

--- modules/afoot.py
+++ modules/afoot.py
@@ -10,5 +10,5 @@ async def runafoot(event):
     await event.delete()
     ridogramuserdetails = await event.get_sender()
     messagelocation = event.to_id
-    ridogramimage = "https://telegra.ph/file/5f0e2d045243c6ab2a4a0.jpg"
-    await client.send_file(messagelocation, ridogramimage, caption=f"Dear {ridogramuserdetails.first_name},\nI'm glad to announce that Ridogram is able to assist you.\n\n╭─⊸ Ridogram Version: 0.9.1\n├─⊸ Python Version: {python_version()}\n╰─⊸ Telethon Version: {version.__version__}\n\nThank You,\nRidogram")
+    ridogramimage = "https://telegra.ph/file/b861edafb332c473a847d.jpg"
+    await client.send_file(messagelocation, ridogramimage, caption=f"Dear {ridogramuserdetails.first_name},\nI'm glad to announce that Snake Rido is able to assist you.\n\n╭─⊸ Rido Version: 0.9.1\n├─⊸ Python Version: {python_version()}\n╰─⊸ Telethon Version: {version.__version__}\n\nThank You Join @SnakeUserBot,\nRidoTeam")
